1cc291d962601ecbaffc37cff25df4af17226d7c
Terrence <chongweijie88@MacBook-Pro-4.local>
2016-09-28 17:42:13 +0800

    ticket in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :image, ImageUploader
   enum role: [:user, :moderator, :admin]
+  has_many :events
+  has_many :comments
 end
