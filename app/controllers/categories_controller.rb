90a2e05e0c5cb02bfbb304cbe5901711c5bbc559
Carson Leong <carsonleong0930@hotmail.com>
2016-10-07 15:47:25 +0800

    final touchup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,14 @@
 class CategoriesController < ApplicationController
 
   def index
+    @categories = Category.all
     @events = Event.search(search_params, page: params[:page], per_page: 15)
   end
 
   def show
     @events = Event.where(category_id: params[:id]).page params[:page]
+    @categories = Category.all
+    @category = Category.find(params[:id])
   end
 
   private
